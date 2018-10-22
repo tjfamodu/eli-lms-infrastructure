608c5b3db457c1b0a2d04a3c3f1680ce4285a7a5
Teju Famodu <tejufamodu@Tejus-MacBook-Pro.local>
2018-10-22 06:19:01 -0400

    updating apache configuration for dev

--- chef/cookbooks/apache/attributes/default.rb
+++ chef/cookbooks/apache/attributes/default.rb
@@ -1,4 +1,4 @@
 node.default['hostname'] = 'canvas.fearless.tech'
 node.default['admin_email'] = 'canvas@fearless.tech'
 node.default['apache']['virtualhosts']['document_root'] = '/var/canvas/public'
-node.default['environment'] = 'test'
+node.default['environment'] = 'development'
